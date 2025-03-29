7e0226f683aa49f8fb78856c289e5eaab95a662b
mattuu0 <tempacountyt@gmail.com>
2025-03-30 02:08:03 +0900

    Nginx と 認証用のコンテナを追加

---
+++ auth/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	router := echo.New()
+
+	router.GET("/", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "Hello, World!")
+	})
+	
+	router.Logger.Fatal(router.Start(":8080"))
+}
