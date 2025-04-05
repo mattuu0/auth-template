6752ee1e990eba7abd0f85050b335a5f0ebf086e
mattuu0 <tempacountyt@gmail.com>
2025-04-05 18:25:54 +0900

    ユーザーが削除されたときにトークンも削除するように変更

--- auth/src/models/user.go
+++ auth/src/models/user.go
@@ -1,13 +1,13 @@
 package models
 
 type User struct {
-    UserID       string       `gorm:"type:varchar(255);primaryKey"` // ユーザーID
-    Name         string       `gorm:"type:varchar(255)"` // ユーザー名
-    Email        string       `gorm:"type:varchar(255);uniqueIndex:idx_users_email,length:255"` // メールアドレス
-    ProvCode     ProviderCode `gorm:"type:varchar(255);index:idx_prov_code,length:255"` // 認証プロバイダコード
-    PasswordHash string       `gorm:"default:''"`        // ハッシュ化されたパスワード
-    CreatedAt    int64        `gorm:"autoCreateTime"`    // ユーザー作成日
-    Sessions     []Session    `gorm:"foreignKey:UserID"` // ユーザーが持つセッション
+	UserID       string       `gorm:"type:varchar(255);primaryKey"` // ユーザーID
+	Name         string       `gorm:"type:varchar(255)"` // ユーザー名
+	Email        string       `gorm:"type:varchar(255);uniqueIndex:idx_users_email,length:255"` // メールアドレス
+	ProvCode     ProviderCode `gorm:"type:varchar(255);index:idx_prov_code,length:255"` // 認証プロバイダコード
+	PasswordHash string       `gorm:"default:''"`        // ハッシュ化されたパスワード
+	CreatedAt    int64        `gorm:"autoCreateTime"`    // ユーザー作成日
+	Sessions     []Session    `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"` // ユーザーが持つセッション
 }
 
 func CreateUser(user *User, ProviderCode ProviderCode) error {
