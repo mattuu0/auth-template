dcda9fb05124d46f85842caac34cf1117c2c0cd7
mattuu0 <tempacountyt@gmail.com>
2025-04-12 01:49:06 +0900

    レンダリングテンプレートを追加

--- auth/src/init.go
+++ auth/src/init.go
@@ -3,14 +3,40 @@ package main
 import (
 	"auth/controllers"
 	"auth/middlewares"
+	"html/template"
+	"io"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (temp *TemplateRenderer) Render(writer io.Writer, name string, data interface{}, ctx echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = ctx.Echo().Reverse
+	}
+
+	return temp.templates.ExecuteTemplate(writer, name, data)
+}
+
 func SetupRouter(router *echo.Echo) {
 	// logger 設定
 	router.Use(middleware.Logger())
+
+	// テンプレート
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+
+	// レンダラー
+	router.Renderer = renderer
 
 	// ルーティング設定
 	// ベーシックユーザーグループ
