e2628bbb87f4250b8d71704840509718a3be0b8b
mattuu0 <tempacountyt@gmail.com>
2025-05-05 18:29:46 +0900

    ログイン後の画面を変更

--- nginx/statics/scripts/home.js
+++ nginx/statics/scripts/home.js
@@ -3,20 +3,23 @@ const auth = new AuthKit('/auth/');
 async function Init() {    
     try {
         // 情報を取得
-        const info = await auth.GetInfo();
+        const userData = await auth.GetInfo();
 
-        if (info == null) {
+        if (userData == null) {
             // ログインにリダイレクト
             window.location.href = './login.html';
             return;
         }
 
-        console.log(info);
+        console.log(userData);
 
-        // hello world 検証
-        const hello = await auth.Get('/app/hello',{});
-
-        console.log(hello);
+        // DOMContentLoaded イベントが発生したらユーザー情報を表示
+        // HTML要素を取得し、データを挿入
+        document.getElementById('user-id').textContent = userData.user_id;
+        document.getElementById('user-name').textContent = userData.name;
+        document.getElementById('user-email').textContent = userData.email;
+        document.getElementById('prov-code').textContent = userData.prov_code;
+        document.getElementById('prov-uid').textContent = userData.prov_uid;
     } catch (error) {
         console.error(error);
         // ログインにリダイレクト
